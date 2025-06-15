e56abdbd14f18e90f94c73cdfb79323b13cdf1ea
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-06-15 11:24:14 +0100

    Tackled the Shoe components, constraining them using flexbox. Done with exercise 4A!

--- src/components/ShoeGrid/ShoeGrid.jsx
+++ src/components/ShoeGrid/ShoeGrid.jsx
@@ -6,14 +6,24 @@ import ShoeCard from '../ShoeCard';
 
 const ShoeGrid = () => {
   return (
-    <Wrapper>
+    <Wrapper>   
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
-      ))}
+        <ShoeWrapper>
+          <ShoeCard key={shoe.slug} {...shoe} />
+        </ShoeWrapper>
+      ))}   
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+`;
+
+const ShoeWrapper = styled.div`
+  flex: 1 1 275px;
+`;
 
 export default ShoeGrid;
