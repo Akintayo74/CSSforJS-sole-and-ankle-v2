6da5a22e0295c70260c90c2d45dfdb2ac9d66262
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-06-15 10:47:59 +0100

    Worked on adapting ShoeIndex and ShoeSidebar to resemble the figma file using flexbox. Done with Exercise 3!

--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -12,6 +12,7 @@ import ShoeGrid from '../ShoeGrid';
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
+      {/* <Spacer size={50}/> */}
       <MainColumn>
         <Header>
           <Title>Running</Title>
@@ -25,8 +26,9 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        <ShoeGrid />
+        {/* <ShoeGrid /> */}
       </MainColumn>
+
       <LeftColumn>
         <Breadcrumbs>
           <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
@@ -38,21 +40,35 @@ const ShoeIndex = ({ sortId, setSortId }) => {
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
+      
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  gap: 32px;
+  align-items: baseline;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  flex-basis: 248px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+  flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  align-items: baseline;
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+  margin-right: auto;
 `;
 
 export default ShoeIndex;
